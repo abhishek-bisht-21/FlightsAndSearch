21ff846b299527e3f3335a9afdb5e8d24d3bad5e
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-19 23:50:58 +0530

    Added Aiport Seeders

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index'); 
 
+const db = require('./models/index');
+// const sequelize = require('sequelize');
+const {City, Airport} = require('./models/index');
+
 const setupAndStartServer = async () => {
 
 	// Create the express object
@@ -15,8 +19,11 @@ const setupAndStartServer = async () => {
 
 	app.use('/api', ApiRoutes);
 
-	app.listen(PORT, () => {
+	app.listen(PORT, async () => {
 		console.log(`Server is up and Running on port ${PORT}`);
+		if(process.env.SYNC_DB){
+		  db.sequelize.sync({alter:true});
+		}
 	});
 }
 
