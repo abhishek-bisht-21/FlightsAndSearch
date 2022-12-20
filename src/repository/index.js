c0ae897e5a3a054295093b192bae718a82c2a28c
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-12-20 20:04:59 +0530

    Added all the repository inside index file to export them

--- src/repository/index.js
+++ src/repository/index.js
@@ -2,6 +2,6 @@ module.exports = {
     CityRepository: require('./city-repository'),
     FlightRespository: require('./flight-repository'),
     AirplaneRespository: require('./airplane-repository'),
-    AirportRespository: require('./airport-respository'),
+    AirportRespository: require('./airport-repository'),
     CrudRespository: require('./crud-repository')
 }
